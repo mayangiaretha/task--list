cd955c52705ffc4c5cfebebe2f6ca81f34fc88b0
mayangiaretha <myaretha2620@gmail.com>
2022-04-27 15:45:35 +0300

    modifying linter to run on prettier

--- ROUTES/items.js
+++ ROUTES/items.js
@@ -1,24 +1,19 @@
 import express from 'express';
 
-import ItemsController from '../controllers/items.js'
+import ItemsController from '../controllers/items.js';
 
 const router = express.Router();
-
 
 // seperation of concerns
 
 router.get('/', ItemsController.getAllItems);
 
-
 router.post('/', ItemsController.createItem);
 
-
-router.get ('/:id', ItemsController.getATask);
+router.get('/:id', ItemsController.getATask);
 
 router.patch('/:id', ItemsController.upDated);
 
-router.delete ('/:id', ItemsController.deleteItem);
-
-
+router.delete('/:id', ItemsController.deleteItem);
 
 export default router;
